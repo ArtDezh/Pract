9a8823bb564e31bbd52ee794aa35047a492df2f0
ArthurD <artur2134@mail.ru>
2023-12-01 20:52:01 +0300

    Новый проект. Реализован

---
+++ src/chat_bot/ChatCommand.java
@@ -0,0 +1,7 @@
+package chat_bot;
+
+public enum ChatCommand {
+    MAP,
+    RECYCLABILITY,
+    BONUS
+}
---
+++ src/chat_bot/MaterialType.java
@@ -0,0 +1,9 @@
+package chat_bot;
+
+public enum MaterialType {
+    PLASTIC,
+    METAL,
+    GLASS,
+    PAPER,
+    UNSORTED_WASTE
+}
