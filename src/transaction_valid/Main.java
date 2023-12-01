ad3574177404c27ec6541b64b8ea5c543bd8b0fe
ArthurD <artur2134@mail.ru>
2023-12-01 21:52:30 +0300

    Новый проект. Два класса.

---
+++ src/transaction_valid/Main.java
@@ -0,0 +1,7 @@
+package transaction_valid;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/transaction_valid/TransactionValidator.java
@@ -0,0 +1,5 @@
+package transaction_valid;
+
+public class TransactionValidator {
+
+}
