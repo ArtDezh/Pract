c300b7c69011040703b8065d82d14ebc0ee3fd45
ArthurD <artur2134@mail.ru>
2023-12-02 12:33:52 +0300

    Закончил с Pet

--- src/pet_pet/Pet.java
+++ src/pet_pet/Pet.java
@@ -1,5 +1,20 @@
 package pet_pet;
 
 public abstract class Pet {
-    
+    private int pawsCount;
+    public void sleep() {
+        System.out.println("Сплю");
+    }
+    public void play() {
+            System.out.println("Играю");
+        }
+    public abstract void giveVoice();
+
+    public int getPawsCount() {
+        return pawsCount;
+    }
+
+    public void setPawsCount(int pawsCount) {
+        this.pawsCount = pawsCount;
+    }
 }
