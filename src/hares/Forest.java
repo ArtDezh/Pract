c0455c1e73cc6a6b72442dcfa769b249363e30e2
ArthurD <artur2134@mail.ru>
2023-12-02 12:04:08 +0300

    Доработал класс Forest.Реализовал класс Main

--- src/hares/Forest.java
+++ src/hares/Forest.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class Forest {
     private List<MountainHare> hares;
     private static String season;
+
+    public Forest(List<MountainHare> hares) {
+        this.hares = hares;
+    }
+
     public static void setSeason(String newSeason) {
         if (newSeason.equals("зима")) {
             MountainHare.setColor("белый");
--- src/hares/Main.java
+++ src/hares/Main.java
@@ -1,7 +1,24 @@
 package hares;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<MountainHare> hares = List.of(
+                new MountainHare(4, 4.4, 120),
+                new MountainHare(7, 3.6, 150),
+                new MountainHare(1, 2.3, 100)
+        );
 
+        System.out.println("В лесу лето!");
+        Forest.setSeason("лето");
+        Forest forestSummer = new Forest(hares);
+
+        System.out.println("Список зайцев:");
+        forestSummer.printHares();
+
+        System.out.println("В лесу зима!");
+        Forest.setSeason("зима");
+        forestSummer.printHares();
     }
 }
