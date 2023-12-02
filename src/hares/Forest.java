37cb58f39e15805aaffa98cd0566db5ee7b65fa1
ArthurD <artur2134@mail.ru>
2023-12-02 11:51:36 +0300

    Закончил класс Forest

--- src/hares/Forest.java
+++ src/hares/Forest.java
@@ -1,6 +1,19 @@
 package hares;
 
+import java.util.List;
+
 public class Forest {
     private List<MountainHare> hares;
+    private static String season;
+    public static void setSeason(String newSeason) {
+        if (newSeason.equals("зима")) {
+            MountainHare.setColor("белый");
+        } else MountainHare.setColor("серо-рыжий");
+    }
 
+    public void printHares() {
+        for (MountainHare hare: hares) {
+            System.out.println(hare);
+        }
+    }
 }
