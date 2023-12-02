829e8b7d454246a8551aa67b41c1e90d36d07de5
ArthurD <artur2134@mail.ru>
2023-12-02 10:28:22 +0300

    Проект закончен.

--- src/transaction_valid/Main.java
+++ src/transaction_valid/Main.java
@@ -1,7 +1,17 @@
 package transaction_valid;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Пожалуйста, введите сумму перевода в рублях.");
+
+        double in = scanner.nextDouble();
+        boolean isValid = TransactionValidator.isValidAmount(in);
+        if (isValid) {
+            System.out.println("Спасибо! Ваш перевод на сумму " + in + " р. успешно выполнен.");
+        }
     }
 }
--- src/transaction_valid/TransactionValidator.java
+++ src/transaction_valid/TransactionValidator.java
@@ -1,5 +1,16 @@
 package transaction_valid;
 
 public class TransactionValidator {
+    public static final double MIN_AMOUNT = 1;
+    public static final double MAX_AMOUNT = 5000;
 
+    public static boolean isValidAmount(double sum) {
+        if (sum >= MIN_AMOUNT && sum <= MAX_AMOUNT) {
+            return true;
+        } else {
+            System.out.println("Минимальная сумма перевода: " + MIN_AMOUNT + " р. Попробуйте ещё раз!");
+            System.out.println("Максимальная сумма перевода: " + MAX_AMOUNT + " р. Попробуйте ещё раз!");
+            return false;
+        }
+    }
 }
